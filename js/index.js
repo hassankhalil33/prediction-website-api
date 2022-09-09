df8730197bd4a3cf51d5016f80bc606668f891b7
Hassan Khalil <hassan.khalil.33@hotmail.com>
2022-09-09 21:26:31 +0300

    Added Random Dog Image on Refresh

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-console.log("HELLO WORLD!");
+fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((data) => {
+        document.querySelector("#img-dog").src = data.message;
+    });
+
