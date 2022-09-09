8cb4846df2e332d7f5277ac8e332539698c9193b
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-10 00:27:07 +0300

    Added API Functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
+const buttonClick = document.querySelector(".btn-submit");
+const imageDog = document.querySelector("#img-dog");
+const guessGender = document.querySelector(".gender");
+const guessAge = document.querySelector(".age");
+const guessNation = document.querySelector(".nation");
+
+function getUserName() {
+    var userName = document.querySelector(".input-name").value;
+    
+    fetch("https://api.genderize.io?name=" + userName)
+        .then((res) => res.json())
+        .then((data) => guessGender.textContent = data.gender);
+        
+    fetch("https://api.agify.io?name=" + userName)
+        .then((res) => res.json())
+        .then((data) => guessAge.textContent = data.age);
+
+    fetch("https://api.nationalize.io?name=" + userName)
+        .then((res) => res.json())
+        .then((data) => guessNation.textContent = data.country[0].country_id
+        + " " + data.country[1].country_id);
+
+    userName = "";
+};
+
+buttonClick.addEventListener("click", getUserName);
+
 fetch("https://dog.ceo/api/breeds/image/random")
     .then((res) => res.json())
-    .then((data) => {
-        document.querySelector("#img-dog").src = data.message;
-    });
-
+    .then((data) => imageDog.src = data.message);
